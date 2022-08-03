c0e5e15f0608413288d878adf9762cfba63f0154
karelinserg <44235235+karelinserg@users.noreply.github.com>
2022-08-03 18:29:04 +0200

    Add files

---
+++ scripts/data.js
@@ -0,0 +1,116 @@
+export const navigation = [
+    {
+        id: 'about',
+        name: 'About',
+    },
+    {
+        id: 'skills',
+        name: 'Skills',
+    },
+    {
+        id: 'work-experience',
+        name: 'Experience',
+    },
+    {
+        id: 'projects',
+        name: 'Projects',
+    },
+    {
+        id: 'education',
+        name: 'Education',
+    },
+    {
+        id: 'languages',
+        name: 'Languages',
+    },
+    {
+        id: 'contact-me',
+        name: 'Contact me',
+    },
+]
+
+
+
+export const skills = [
+    {
+        name: 'HTML',
+        imgUrl: 'HTML',
+    },
+    {
+        name: 'CSS',
+        imgUrl: 'CSS3',
+    },
+    {
+        name: 'SASS',
+        imgUrl: 'SASS',
+    },
+    {
+        name: 'JavaScript',
+        imgUrl: 'JS',
+    },
+    {
+        name: 'React',
+        imgUrl: 'REACT',
+    },
+    {
+        name: 'Redux',
+        imgUrl: 'REDUX',
+    },
+    {
+        name: 'React Router',
+        imgUrl: 'REACTROUTER',
+    },
+    {
+        name: 'Git',
+        imgUrl: 'GIT',
+    },
+    {
+        name: 'GitHub',
+        imgUrl: 'GITHUB',
+    },
+    {
+        name: 'Pug',
+        imgUrl: 'PUG',
+    },
+    {
+        name: 'Webpack',
+        imgUrl: 'WEBPACK',
+    },
+    {
+        name: 'npm',
+        imgUrl: 'NPM',
+    },
+    {
+        name: 'Figma',
+        imgUrl: 'FIGMA',
+    },
+    {
+        name: 'Styled Components',
+        imgUrl: 'STYLEDCOMPONENTS',
+    }
+]
+
+export const languages = [
+    {
+        name: 'English',
+        level: 'Pre-Intermediate A2',
+    }
+]
+
+export const contactLinks = [
+    {
+        link: 'https://github.com/ImennoArtemiy',
+        img: './img/info/github.png',
+        alt: 'GitHub',
+    },
+    {
+        link: 'https://www.linkedin.com/in/artem-litovchenko/',
+        img: './img/info/linkedin.png',
+        alt: 'Linkedin',
+    },
+    {
+        link: 'https://t.me/imennoartemiy',
+        img: './img/info/telegram.png',
+        alt: 'Telegram',
+    }
+]
